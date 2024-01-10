4fe619ea4232c40115ad56b6e1237c50bd5f8376
Chuck Pathanjali <98570028+chuckp22@users.noreply.github.com>
2024-01-10 17:54:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo '[[ -v GH_GH_PAT ]] && export GITHUB_TOKEN=$GH_GH_PAT' >> ~/.bashrc
 test -f ~/.bash_profile && echo "set -o vi" >> ~/.bash_profile || echo "set -o vi" > ~/.bash_profile
+
+base_path=$(cd $(dirname $0) && pwd)
+cp -av \
+  $base_path/.bashrc \
+  $base_path/.bash_profile \
+  $base_path/.vimrc \
+  $base_path/bin \
+  ~
